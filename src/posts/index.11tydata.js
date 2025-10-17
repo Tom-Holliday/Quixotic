c75c6b349eeb194c24e828015bb8b73c192d5a45
Tom Holliday <tom.holliday@hotmail.com>
2025-10-17 14:56:21 +0100

    Resolve pull conflict

--- src/posts/index.11tydata.js
+++
@@ -1,9 +0,0 @@
-// src/posts/index.11tydata.js
-module.exports = {
-  layout: "post.njk",
-  tags: ["post"],
-  eleventyComputed: {
-    permalink: (data) => `/posts/${data.page.fileSlug}/`,
-    byline: (data) => data.byline || "Tom Holliday",
-  },
-};
